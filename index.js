ad3d310fa01872ef8861e5efa7817233e93a8113
Oghovese Imetowa <itzoghuski@gmail.com>
2024-04-23 21:12:24 +0100

    finished the project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const imgElement = document.getElementById("imgElement")
 const title = document.getElementById('title')
 const description = document.getElementById('description')
 
+//fecht
 fetch(Data)
     .then(response => response.json())
     .then(data => {
@@ -10,12 +11,32 @@ fetch(Data)
         imgElement.src = data[0].image;
         imgElement.style.display = 'block';
         title.innerHTML = data[0].title
-        description.innerHTML = data[0].description
+        description.innerHTML += data[0].description
 
     })
     .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
-    })
+    });
+
+const list = document.getElementById('list')
+const btn = document.getElementById('line-container')
+
+btn.addEventListener ('click', toggleMiniBar) 
+
+function toggleMiniBar() {
+    if (list.style.display === "none") {
+      list.style.display = "block";
+    } else {
+      list.style.display = "none";
+    }
+  }
+
+// document.addEventListener ('click', (e) => {
+//     if (list.style.display === "block"){
+//         list.style.display = "none";
+//         console.log('it worked');
+//     }
+// })
 
 
     
